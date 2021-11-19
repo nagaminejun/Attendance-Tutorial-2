059e8f4d20ba982c4e255b36c5da129b75d55fe6
長嶺純 <nagaminejun1988@gmail.com>
2021-11-19 00:05:31 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users #この1行を記述するだけで全てのリソースを扱える./users/1というURLを有効にする
-  
+  resources :users do #この1行を記述するだけで全てのリソースを扱える./users/1というURLを有効にする
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
